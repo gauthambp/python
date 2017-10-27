f833c4a7ec843d475e5a7740a4130e4f9137b58a
Gautham Pujari <gautham.pujari@gmail.com>
2017-10-27 15:47:28 +0530

    added the python path variable

--- oelist.py
+++ oelist.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 #THis program will check the appserver status on the server, then will present the appservers as choice to be either restarted, stopped or queried.
 import os
 list=os.popen("ver=102B . /usr/local/dlc/bin/dlcverset;nsman -i NS1 -port 20943 -q|awk '{print $2}'|grep -e AS -e WS").read()
@@ -6,6 +8,7 @@ listvar=list.splitlines()
 <<<<<<< HEAD
 newlistvar=[x.split('.')[-1] for x in listvar]
 #Added on 27Oct to list the appservers
+print "The following appservers are seen on the server"
 for i,j in enumerate(newlistvar):
     print i,j
 #dummy comment

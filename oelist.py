d79942bfe950e80298899821e8d324b43b123fa0
Gautham Pujari <gautham.pujari@gmail.com>
2017-11-01 16:11:06 +0530

    added on 01 nov 2017

--- oelist.py
+++ oelist.py
@@ -11,4 +11,23 @@ newlistvar=[x.split('.')[-1] for x in listvar]
 print "The following appservers are seen on the server"
 for i,j in enumerate(newlistvar):
     print i,j
+    k[i]=j
+#Next step is to run the command based on selections
+print "Select the appserver to be used"
+#The appserver needs to be stopped started or queired"
+i = input("select the appserver")
+myoutput=os.popen("ver=102B . /usr/local/dlc/bin/dlcverset;nsman -i $k[i] -port 20943 -q).read()
+print myoutput
+
+#The appserver needs to be stopped started or queired"
+i = input("select the appserver")
+myname=k[i]
+print myname
+print "%s" % myname
+myoutput=os.popen("ver=102B . /usr/local/dlc/bin/dlcverset;nsman -i" + ' ' + myname + "-port 2094
+3 -q").read()
+print myoutput
+
+#dummy comment
+
 #dummy comment

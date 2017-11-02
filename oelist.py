6eabb4eb5e83e0c75b312e7222e5668927c1556a
gauthambp <gautham.pujari@gmail.com>
2017-11-02 11:40:45 +0530

    Completely re-written

--- oelist.py
+++ oelist.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python
 
-#THis program will check the appserver status on the server, then will present the appservers as choice to be either restarted, stopped or queried.
+#This program will check the appserver status on the server, then will present the appservers as choice to be either restarted, stopped or queried.
 import os
 list=os.popen("ver=102B . /usr/local/dlc/bin/dlcverset;nsman -i NS1 -port 20943 -q|awk '{print $2}'|grep -e AS -e WS").read()
 listvar=[]
 listvar=list.splitlines()
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 newlistvar=[x.split('.')[-1] for x in listvar]
 #Added on 27Oct to list the appservers
 print "The following appservers are seen on the server"
+#k used to create a dictionary holder
+k={}
 for i,j in enumerate(newlistvar):
     print i,j
-<<<<<<< HEAD
     k[i]=j
-#Next step is to run the command based on selections
-print "Select the appserver to be used"
-#The appserver needs to be stopped started or queired"
-i = input("select the appserver")
-myoutput=os.popen("ver=102B . /usr/local/dlc/bin/dlcverset;nsman -i $k[i] -port 20943 -q).read()
-print myoutput
 
+#print "Select the appserver to be used"
 #The appserver needs to be stopped started or queired"
-i = input("select the appserver")
+i = input("Select the appserver to be used\n")
 myname=k[i]
+print "You have selected the following Appserver"
 print myname
-print "%s" % myname
-myoutput=os.popen("ver=102B . /usr/local/dlc/bin/dlcverset;nsman -i" + ' ' + myname + "-port 2094
-3 -q").read()
+
+myoutput=os.popen("ver=102B . /usr/local/dlc/bin/dlcverset;nsman -i %s -port 20943 -q" %myname).read()
 print myoutput
 
 #dummy comment
-
-#dummy comment
-=======
-
->>>>>>> origin/master
